186f6cd38684972768165e1c4a53489dcbd626f3
DESKTOP-27VVUBG\chats <oocjh200@gmail.com>
2018-08-12 09:36:39 +0900

    .

--- Kgsh_meal_Xamarin/ChoiceProcess.cs
+++ Kgsh_meal_Xamarin/ChoiceProcess.cs
@@ -14,7 +14,7 @@ namespace Kgsh_meal_Xamarin
 {
     class ChoiceProcess:Thread
     {
-        private boolean _good;
+        private bool _good;
         private int _time;
         private SeekBar _choice;
 
@@ -24,21 +24,21 @@ namespace Kgsh_meal_Xamarin
         {
         public void handleMessage(Message msg)
         {
-            switch (msg.what)
+            switch (msg.What)
             {
                 case 0:
                     {
-                        Toast.MakeText(MealMainActivity.instance.getContext(), "오늘 식단에  투표해주셔서 감사합니다.", Toast.LENGTH_SHORT).show();
+                        Toast.MakeText(MealMainActivity.instance.getContext(), "오늘 식단에  투표해주셔서 감사합니다.", ToastLength.Short).show();
                     }
                     break;
                 case 1:
                     {
-                        Toast.MakeText(MealMainActivity.instance.getContext(), "오늘 이 식단에는 이미 투표하셨습니다.", Toast.LENGTH_SHORT).show();
+                        Toast.MakeText(MealMainActivity.instance.getContext(), "오늘 이 식단에는 이미 투표하셨습니다.", ToastLength.Short).show();
                     }
                     break;
                 case 2:
                     {
-                        Toast.MakeText(MealMainActivity.instance.getContext(), "해당 날짜에만 투표해주세요.", Toast.LENGTH_SHORT).show();
+                        Toast.MakeText(MealMainActivity.instance.getContext(), "해당 날짜에만 투표해주세요.", ToastLength.Short).show();
                     }
                     break;
             }
